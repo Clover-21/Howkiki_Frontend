02fac22a8f6ee2099d256966cda5574e85aa17f2
LeeJooyeon <smilejuju@ewha.ac.kr>
2025-01-28 14:26:34 +0900

    feat : #9 테이블 관리 페이지 tablebox 생성

--- src/pages/manager/tableManage.jsx
+++ src/pages/manager/tableManage.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Header from "../../components/Header";
+import {
+  TableBoxContainer,
+  TableBox,
+} from "../../styles/manager/tableManage.module";
 
 export default function TableManagePage() {
+  // 임의로 7개로 지정
+  const tables = Array.from({ length: 7 }, (_, index) => ({
+    id: index + 1,
+    name: `${index + 1}번`,
+  }));
+
   return (
     <>
       <Header />
+      <TableBoxContainer>
+        {tables.map((table) => (
+          <TableBox key={table.id}>{table.name}</TableBox>
+        ))}
+      </TableBoxContainer>
     </>
   );
 }
