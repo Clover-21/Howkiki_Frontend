a05d5c173ae5f04be969edc8c3fa14cf99fd0cf3
LeeJooyeon <smilejuju@ewha.ac.kr>
2025-02-03 09:14:01 +0900

    feat : #9 전체 주문 조회 페이지 생성

--- src/pages/manager/fullOrder.jsx
+++ src/pages/manager/fullOrder.jsx
@@ -1,12 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "../../components/Header";
 import SideBar from "../../components/SideBar";
+import {
+  ListContainer,
+  OrderContainer,
+  OrderContent,
+  MenuContainer,
+  MenuContent,
+  TableNum,
+  MenuName,
+  MenuQuantity,
+} from "../../styles/manager/orderWaiting.module";
 
-export default function FullOrderPage() {
+const host =
+  window.location.hostname === "localhost"
+    ? "http://15.164.233.144:8080"
+    : "api";
+
+export const apiClient = axios.create({
+  baseURL: host,
+});
+
+export default function PayCompletePage() {
+  const [orderData, setOrderData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // 주문 데이터 가져오기 함수
+  const fetchOrderData = async () => {
+    try {
+      const response = await apiClient.get(`${host}/stores/1/orders/all`);
+      setOrderData(response.data);
+    } catch (error) {
+      console.error("주문 데이터 가져오기 실패:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrderData();
+  }, []);
+
   return (
     <>
       <Header />
-      <SideBar />
+      <ListContainer>
+        <SideBar />
+        <OrderContainer>
+          {orderData?.data.orders?.length > 0 ? (
+            orderData.data.orders.map((order, i) => (
+              <OrderContent key={i}>
+                <TableNum>{order.tableNumber}번</TableNum>
+                <MenuContainer>
+                  {order.menuSummary?.map((menu, i) => (
+                    <MenuContent key={i}>
+                      <MenuName>{menu.menuName}</MenuName>
+                      <MenuQuantity>{menu.quantity}</MenuQuantity>
+                    </MenuContent>
+                  ))}
+                </MenuContainer>
+              </OrderContent>
+            ))
+          ) : (
+            <div>주문이 없습니다.</div>
+          )}
+        </OrderContainer>
+      </ListContainer>
     </>
   );
 }
