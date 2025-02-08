48fcf8c2293e8f6ce7000704d4b7203f21db4db6
LeeJooyeon <smilejuju@ewha.ac.kr>
2025-02-08 16:09:49 +0900

    feat : #15 orderbox swiper 적용

--- src/pages/chatbot/orderSummary.jsx
+++ src/pages/chatbot/orderSummary.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Autoplay, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import {
   SummaryContainer,
   SummaryContentWrap,
@@ -10,16 +12,50 @@ import {
 import StatusBar from "../../components/chatbot/StatusBar";
 import OrderBox from "../../components/chatbot/OrderBox";
 import chatbot from "../../assets/icon/chatbot.svg";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 
 export default function OrderSummaryPage() {
   const navigate = useNavigate();
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <SummaryContainer>
       <SummaryContentWrap>
         <OrderContent>
           <StatusBar />
-          <OrderBox />
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            <Swiper
+              modules={[Pagination, Autoplay]}
+              slidesPerView={1.2}
+              spaceBetween={-40}
+              onSlideChange={(e) => setActiveIndex(e.activeIndex)}
+              autoplay={{
+                delay: 4500,
+                disableOnInteraction: false,
+              }}
+              speed={700}
+              pagination={{
+                clickable: true,
+              }}
+            >
+              <SwiperSlide style={{ marginLeft: "40px" }}>
+                <OrderBox />
+              </SwiperSlide>
+              <SwiperSlide>
+                <OrderBox />
+              </SwiperSlide>
+              <SwiperSlide>
+                <OrderBox />
+              </SwiperSlide>
+            </Swiper>
+          </div>
           <IconWrap>
             <ChatIcon src={chatbot} onClick={() => navigate("/chatbot")} />
           </IconWrap>
