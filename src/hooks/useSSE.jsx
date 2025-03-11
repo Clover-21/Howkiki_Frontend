bb4559fbff8539a4109e2f49a506a2042468cea5
LeeJooyeon <smilejuju@ewha.ac.kr>
2025-03-11 15:23:14 +0900

    feat : sse 재연결 코드 추가

--- src/hooks/useSSE.jsx
+++ src/hooks/useSSE.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { EventSourcePolyfill } from "event-source-polyfill";
 
 const API_URL = process.env.REACT_APP_API_URL;
-
 const activeSSEConnections = new Map();
 
 export function closeSSEConnection(token) {
@@ -20,38 +19,44 @@ export default function useSSE(token) {
   useEffect(() => {
     if (!token || activeSSEConnections.has(token)) return;
 
-    const eventSource = new EventSourcePolyfill(
-      `${API_URL}/notification/subscribe`,
-      {
-        headers: {
-          sessionToken: token,
-          Accept: "text/event-stream",
-          "Cache-Control": "no-cache",
-          Connection: "keep-alive",
-        },
-        heartbeatTimeout: 45000,
-      }
-    );
+    const connectSSE = () => {
+      const eventSource = new EventSourcePolyfill(
+        `${API_URL}/notification/subscribe`,
+        {
+          headers: {
+            sessionToken: token,
+            Accept: "text/event-stream",
+            "Cache-Control": "no-cache",
+            Connection: "keep-alive",
+          },
+          heartbeatTimeout: 45000,
+        }
+      );
 
-    eventSource.onopen = () => {
-      console.log(`SSE 연결 성공! (토큰: ${token})`);
-      activeSSEConnections.set(token, eventSource);
+      eventSource.onopen = () => {
+        console.log(`SSE 연결 성공! (토큰: ${token})`);
+        activeSSEConnections.set(token, eventSource);
+      };
+
+      eventSource.addEventListener("notification", (event) => {
+        try {
+          const newNotice = JSON.parse(event.data);
+          setNotice(newNotice);
+          setIsOpen(true);
+        } catch (error) {
+          console.error("알림 오류:", error);
+        }
+      });
+
+      eventSource.onerror = (error) => {
+        console.error("SSE 연결 오류:", error);
+        closeSSEConnection(token);
+
+        setTimeout(connectSSE, 3000);
+      };
     };
 
-    eventSource.addEventListener("notification", (event) => {
-      try {
-        const newNotice = JSON.parse(event.data);
-        setNotice(newNotice);
-        setIsOpen(true);
-      } catch (error) {
-        console.error("알림 오류:", error);
-      }
-    });
-
-    eventSource.onerror = (error) => {
-      console.error("SSE 연결 오류:", error);
-      closeSSEConnection(token);
-    };
+    connectSSE();
 
     return () => {
       closeSSEConnection(token);
