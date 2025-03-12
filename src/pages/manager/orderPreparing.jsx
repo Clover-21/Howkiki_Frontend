edf7a9cde4fe960752459158dddd8ecced36a73b
LeeJooyeon <smilejuju@ewha.ac.kr>
2025-03-12 09:51:17 +0900

    chore

--- src/pages/manager/orderPreparing.jsx
+++ src/pages/manager/orderPreparing.jsx
@@ -25,11 +25,11 @@ import {
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-const host = window.location.hostname === "localhost" ? API_URL : "api";
-
-export const apiClient = axios.create({
-  baseURL: host,
-});
+const host =
+  window.location.hostname === "localhost"
+    ? API_URL
+    : `${window.location.origin}/api`;
+export const apiClient = axios.create({ baseURL: host });
 
 const override = {
   display: "block",
