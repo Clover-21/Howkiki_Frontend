5f30a540e54f8d7d83c83cbca56f3142ec79a394
LeeJooyeon <smilejuju@ewha.ac.kr>
2025-03-13 14:28:57 +0900

    chore : proxy 코드 변경

--- netlify/functions/proxy.jsx
+++ netlify/functions/proxy.jsx
@@ -1,12 +1,32 @@
+const fetch = require("node-fetch");
+
 exports.handler = async (event) => {
   const API_URL = process.env.REACT_APP_API_URL;
   const path = event.path.replace("/api/", "");
+  const fullUrl = `${API_URL}/${path}`;
+  const headers = { "Content-Type": "application/json" };
 
-  return {
-    statusCode: 302,
-    headers: {
-      Location: `${API_URL}/${path}`,
-    },
-    body: "",
-  };
+  try {
+    const response = await fetch(fullUrl, {
+      method: event.httpMethod,
+      headers,
+      body: event.body,
+    });
+
+    const data = await response.text();
+
+    return {
+      statusCode: response.status,
+      headers: { "Content-Type": "application/json" },
+      body: data,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Internal Server Error",
+        details: error.message,
+      }),
+    };
+  }
 };
